0aa80f521b49a173b32766e668661f8d18f7f764
Phil Ganney <philipganney@gmail.com>
2022-01-07 23:29:57 +0000

    Info/license button + copyright info at bottom
    I did this early on, because it's a little bit of interactivity, but it's also quite simple - though less simple than it could have been because I wanted it to feel sleek.
    
    I feel I've set a really good precedent for this project. It feels sleek and designed, yet retro and "developer-y".
    
    The focus outlines, and applying focus to the back button, is needed for keyboard users. Making it lime green dashed, really helps the developer feel. Having the back button exist, and be where it is, was needed to keep the sleek design - I initially tried using a toggle button, but keeping the info/license button on screen didn't  look good in any of a few forms that came to mind.

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
 //btn.onclick = function() {}
 /*by "via class" we mean by removing or adding a class called "hidden" that sets css display to none
 Use in conjuction with hideViaClass(id) and a css class called "hidden".
-This is a slight change in how I've done this technique on other projects - there's no "showing" class, will also need specific selectors written in.
+This is a slight change in how I've done this technique on other projects - there's no "showing" class
 */
 function showViaClass(id){
   document.getElementById(id).classList.remove("hidden");
 }
 function hideViaClass(id){
 	document.getElementById(id).classList.add("hidden");
+}
+function toggleHiddenViaClass(id){
+	if(document.getElementById(id).classList.contains("hidden")){
+		document.getElementById(id).classList.remove("hidden");
+	} else {
+		document.getElementById(id).classList.add("hidden");
+	}
+}
+function toggleHideIfSmallScreen(id){
+	if(document.getElementById(id).classList.contains("hideIfSmallScreen")){
+		document.getElementById(id).classList.remove("hideIfSmallScreen");
+	} else {
+		document.getElementById(id).classList.add("hideIfSmallScreen");
+	}
+}
+
+
+function infoBtn(){
+	hideViaClass("topRow");
+	hideViaClass("lowRow");
+	showViaClass("info");
+	hideViaClass("mainSubtitle");
+	showViaClass("infoSubtitle");
+	showViaClass("backBtn");
+	document.getElementById("backBtn").onclick = function() {closeInfo();}
+	document.getElementById("backBtn").focus();
+}
+
+function closeInfo(){
+	showViaClass("topRow");
+	showViaClass("lowRow");
+	hideViaClass("info");
+	showViaClass("mainSubtitle");
+	hideViaClass("infoSubtitle");
+	hideViaClass("backBtn");
+	document.getElementById("backBtn").onclick = function() {}
 }
