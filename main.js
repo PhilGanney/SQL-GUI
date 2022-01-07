068f46d4f19a145c05b47ea37f52bd98a235f9ad
Phil Ganney <philipganney@gmail.com>
2022-01-07 12:58:56 +0000

    Initial screen mobile layout, Basic
    A good way to start is to take an existing simple layout that works and is roughly like what is needed for the home page, and adapt it into the skeleton of the home page
    
    In this case I've used my ButtonPop project as the starting point, stripped out everything I know I won't need

---
+++ main.js
@@ -0,0 +1,11 @@
+//btn.onclick = function() {}
+/*by "via class" we mean by removing or adding a class called "hidden" that sets css display to none
+Use in conjuction with hideViaClass(id) and a css class called "hidden".
+This is a slight change in how I've done this technique on other projects - there's no "showing" class, will also need specific selectors written in.
+*/
+function showViaClass(id){
+  document.getElementById(id).classList.remove("hidden");
+}
+function hideViaClass(id){
+	document.getElementById(id).classList.add("hidden");
+}
