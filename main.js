e67bef897bf511a5f3ec805cc7495344bb33d966
Phil Ganney <philipganney@gmail.com>
2022-01-08 18:43:39 +0000

    Better state management
    Phew that's done, and the code base looks so much better.
    
    I won't have to think about the back button for a long time now - probably not until I decide I'm ready to map that button to keyboard buttons.

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var globals = {
-	subtitlesStack: [],
-	
+	"mainPanelStack": ["startScreen"], //a stack for keeping track of where the back button should take the user
+	"screenData": { //this is divided up by the Div name for each screen in the mainPanel, see the subtitle for the H2 text in the titlebar for each screen
+		"startScreen": { //the first screen that shows when you start SQL GUI
+			"subtitle": "by Phil Ganney"
+		},
+		"info": { //Can be arrived at from the startScreen
+			"subtitle": "Info / license"
+		},
+		"newDbPg1": { //Can be arrived at from the startScreen
+			"subtitle": "New Database"
+		},
+		"dbNamer": { //Can be arrived at from newDbPg1
+			"subtitle": "Name your DB"
+		},
+		"tableDesigner": { //
+			"subtitle": "New Table"
+		}
+		
+	}
 }
 
 /*by "via class" we mean by removing or adding a class called "hidden" that sets css display to none
@@ -28,47 +45,66 @@ function toggleHideIfSmallScreen(id){
 	}
 }
 
-
-function infoBtn(){
-	hideViaClass("startScreen");
-	showViaClass("info");
-	hideViaClass("mainSubtitle");
-	showViaClass("infoSubtitle");
-	showViaClass("backBtn");
-	document.getElementById("backBtn").onclick = function() {closeInfo();}
-	document.getElementById("backBtn").focus();
+function swapElems(oldEl, newEl){
+	console.log("swapElems(oldEl, newEl) called, oldEl = " + oldEl + ", newEl = " + newEl);
+	//hide oldEl
+	document.getElementById(oldEl).classList.add("hidden");
+	//show newEl
+	document.getElementById(newEl).classList.remove("hidden");
 }
 
-function closeInfo(){
-	showViaClass("startScreen");
-	hideViaClass("info");
-	showViaClass("mainSubtitle");
-	hideViaClass("infoSubtitle");
-	hideViaClass("backBtn");
-	document.getElementById("backBtn").onclick = function() {}
+function swapSubtitle(newSubtitle){
+	console.log("swapSubtitle(newSubtitle) started, newSubtitle = " + newSubtitle);
+	document.getElementById("subtitle").innerHTML = newSubtitle;
+	console.log("swapSubtitle(newSubtitle) finished");
 }
 
-function newDbBtn(){
-	hideViaClass("startScreen");
-	showViaClass("newDb");
-	hideViaClass("mainSubtitle");
-	showViaClass("newDbSubtitle");
-	showViaClass("backBtn");
-	document.getElementById("backBtn").onclick = function() {closeNewDb();}
-	document.getElementById("backBtn").focus();
+function changeScreenForward(nextScreen){
+	console.log("changeScreenForward(nextScreen) started, nextScreen = " + nextScreen);
+	
+	/*we want to swap the screen we're on, with the given screen
+		the screen we're on will be the last item in "mainPanelStack"
+		to get the last item of an array we do arrayName[arrayName.length - 1]
+	*/
+	let stack = globals.mainPanelStack; //"stack" exists to simplify code in this function
+	swapElems(stack[stack.length - 1], nextScreen); //Actually swaps the screen we're on
+	
+	/*Now we want to swap the subtitle 
+		- but instead of swapping which element is visible, we swap the inner html of the subtitle*/
+	swapSubtitle(globals.screenData[nextScreen].subtitle); //swap the subtitle 
+	
+	//when we're going forward, presumably the back button will always need to show
+	showViaClass("backBtn"); //show the back button now that we're not on the top screen
+	document.getElementById("backBtn").focus(); //give the backBtn focus, reasons: 1) best place for keyboard users to start from since it's at the top of the view 2)focus makes it clear that they can easily go back using it 3)lets keyboard users immediatly tap enter if they made a mistake going in there or where only "exploring" - making it effortless to explore and reducing the irritation caused by making a mistake
+	
+	//Then we need to add to the end of the mainPanelStack
+	globals.mainPanelStack.push(nextScreen);
+	
+	console.log("changeScreenForward(nextScreen) finished, mainPanelStack = " + globals.mainPanelStack);
 }
 
-function closeNewDb(){
-	showViaClass("startScreen");
-	hideViaClass("newDb");
-	showViaClass("mainSubtitle");
-	hideViaClass("newDbSubtitle");
-	hideViaClass("backBtn");
-	document.getElementById("backBtn").onclick = function() {}
-}
+function changeScreenBackward(){
+	console.log("changeScreenBackward() started");
+	/* 	To do anything in this function:
+			we need the screen we're on,
+				found at the end of the mainPanelStack
+				stack[stack.length - 1]
+			and the screen we need to go "back" to
+				found just before the end of the mainPanelStack
+				stack[stack.length - 2]
+	*/
+	let stack = globals.mainPanelStack; //"stack" exists to simplify code in this function
+	
+	swapElems(stack[stack.length - 1], stack[stack.length - 2]); //swap the screen we're on
+	//then we need to swap the subtitle
+	swapSubtitle(globals.screenData[stack[stack.length - 2]].subtitle);
 
+	stack.pop(); //VERY IMPORTANT at least to this implementation of managing views via a stack
+		
+	/*If we've just returned to the homepage, we want to hide the backBtn*/
+	if (stack.length == 1){
+		hideViaClass("backBtn");
+	}
+	console.log("changeScreenBackward() finished, mainPanelStack = " + globals.mainPanelStack);
 
-function showMainScreen(){
-	showViaClass("designer");
-	hideViaClass("newDb");
 }
